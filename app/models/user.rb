30eccaa7f402371851b490eb0b30f8e3cf726391
Elizabeth <mwendemuthusi6@gmail.com>
2023-07-06 11:57:21 +0300

    Feat: update user's relationship with categories and group models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :tasks
   has_one :profile
+  has_many :categories, through: :tasks
+  has_many :groups, through: :tasks
   validates :email, uniqueness: true
   validates :password, length: {minimum:5, maximum:9}
 end
