e8628da7e3a3dc50378cd03388e5898c0d5f9cf4
Elizabeth <mwendemuthusi6@gmail.com>
2023-07-25 07:53:14 +0300

    Fix: add start_time and end_time  to serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,3 +1,3 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :category_id, :group_id, :user_id, :title, :start_date, :end_date, :status, :label, :reminder, :description
+  attributes :id, :category_id, :group_id, :user_id, :title, :start_date, :end_date, :status, :label, :reminder, :description, :start_time, :end_time
 end
